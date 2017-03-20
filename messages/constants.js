83be048b6ebace7c7d05a9c2e2ac49baf31fd333
Amr Fouad <amr.fouad@hotmail.co.uk>
2017-03-20 11:18:32 +1100

    update bot url

--- messages/constants.js
+++ messages/constants.js
@@ -3,7 +3,7 @@ module.exports={
     domain:'53831c5e-59fa-44d7-8854-676856f2fada',
     secret:'zKmhpFOw6yei1ab0sFkvIzjTSa4R48NxRZhlPaVAKwE=',
     subscriptionId:'2cc6c2e8-e5d0-4611-85f2-adea3cccb893',
-    notificationUrl:'http://localhost:3978/api/proactive',
+    notificationUrl:'https://SMSAzureBot.azurewebsites.net/api/proactive',
     deploymentName:'SMSMTDeployment01',
     resourceGroupName:'SMSMTRG02'
 }
